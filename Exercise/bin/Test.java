bdc2f73cc7ae57385e7a0975ef6cd952be18b530
lyon-sun <sunly917@gmail.com>
2013-02-26 21:40:09 +0200

    add another variable and print it out

--- Exercise/bin/Test.java
+++ Exercise/bin/Test.java
@@ -1,8 +1,11 @@
 class Test {
 	public static void main(String[] args) {
-		//define variables
+		//define a variable
 		String s = "this is a string";
 		System.out.println("I am a string, which is: " + s);
+		//define another variable
+		String s2 = "this is another string";
+		System.out.println("I am another string, call me at " + s2);
 		System.out.println("hello, I was changed and added into the repository.");
 	}
 }
