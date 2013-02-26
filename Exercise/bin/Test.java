e6514068b0714d0cf5a077db4bd4e09323601384
lyon-sun <sunly917@gmail.com>
2013-02-26 21:18:54 +0200

    commit comments
    alright, so every line has to be committed

--- Exercise/bin/Test.java
+++ Exercise/bin/Test.java
@@ -1,5 +1,6 @@
 class Test {
 	public static void main(String[] args) {
+		//define variables
 		String s = "this is a string";
 		System.out.println("I am a string, which is: " + s);
 		System.out.println("hello, I was changed and added into the repository.");
