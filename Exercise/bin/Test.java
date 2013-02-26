cb9bcf683b9d2ec51b8e2fba1e0135363dd73bed
lyon-sun <sunly917@gmail.com>
2013-02-26 21:17:38 +0200

    add one local variable and print it out

--- Exercise/bin/Test.java
+++ Exercise/bin/Test.java
@@ -1,5 +1,7 @@
 class Test {
 	public static void main(String[] args) {
+		String s = "this is a string";
+		System.out.println("I am a string, which is: " + s);
 		System.out.println("hello, I was changed and added into the repository.");
 	}
 }
